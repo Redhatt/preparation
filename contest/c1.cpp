f3fc7a33d9692e167617a51698964f2ff74ccff9
Danish.Amin <Danish.Amin@ril.com>
2022-07-08 21:07:14 +0530

    updates

--- contest/c1.cpp
+++ contest/c1.cpp
@@ -1,49 +1,71 @@
 #include <bits/stdc++.h>
-#define len(a) a.size()
-#define pb push_back
-#define mkp make_pair
-#define F first
-#define S second
-#define all(a) a.begin(), a.end()
-using namespace std;
-#define newline cout<<"\n";
-#define fine cout<<"FINE"; newline
-#define debug(x) cout<<"x: "<<x;newline
-typedef long long int ll;
-#define rep(i,a,b) for (ll i=a; i<b; i++)
-typedef vector<ll> vi;
-typedef vector<vi> vvi;
-typedef vector<string> vs;
-typedef vector<bool> vb;
-typedef pair<ll, ll> pll;
-typedef vector<pll> vp;
-long long int inf = 100000000000000000;
-ll mod = 1000000007;
-void printvi(vi v){for (auto x: v)  cout<<x<<" ";newline}
-void printvb(vb v){for (auto x: v)  cout<<x<<" ";newline}
-void printvvi(vvi v){for (auto x: v){for (auto y:x)  cout<<y<<" ";newline}}
-void printvs(vs v){for (auto x: v)  cout<<x<<" ";newline}
-void printvp(vp v){for (auto &x: v) {cout<<x.F<<" "<<x.S;newline}}
 
+using namespace std;
+
+vector<vector<int>> graph;
+vector<int> in, out, flat, bit, value;
+int N, n, q;
+
+
+void dfs(int node, int parent, int& timer)
+{
+    timer++;
+    in[node] = timer;
+    flat[timer] = node;
+
+    for (int nbr: graph[node]) {
+        if (nbr == parent) continue;
+        dfs(nbr, node, timer);
+    }
+    timer++;
+    out[node] = timer;
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
-	// Code here ------------------------
-	
-	int arr[] = {3,2,1};
-	sort(arr, arr+3);
-	cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
-	cout<<pow(2, 10);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    clock_t start, end;
+    start = clock();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 
 
-	cout<<"working";
+    // Code here ------------------------
+    int i, a, b;
+    cin>>n>>q;
 
-	// ----------------------------------
-	return 0;
+    N = n + 1;
+    graph.resize(N);
+    in.assign(N, 0);
+    out.assign(N, 0);
+    value.assign(N, 0);
+    flat.assign(3*N, 0);
+    bit.assign(3*N + 1, 0);
+
+    for (i=0; i<n; i++)
+        cin>>value[i];
+
+    for (i=0; i<n-1; i++) {
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    int timer = 0;
+    dfs(1, -1, timer);
+
+    for (int x: flat)
+        cout<<x<<" ";
+
+    // ----------------------------------
+
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC/1000); 
+    cout << "\n||Time = " << fixed << time_taken << setprecision(5); 
+    cout << "ms||" << endl; 
+    return 0;
 }
