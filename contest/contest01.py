16fc8dfbb3f7634e4a22971463f03817131cac04
danish.amin <danish.amin@ril.com>
2024-02-22 10:13:33 +0530

    updated

--- contest/contest01.py
+++ contest/contest01.py
@@ -1,56 +1,36 @@
 from debug import *
 import sys;input = sys.stdin.readline
 # import sys, os, io; input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+from collections import defaultdict
+from math import ceil
+
+
 S = lambda :input().strip()
 L = lambda :list(map(int, input().split()))
 I = lambda :int(input().strip())
 T = lambda :map(int, input().split())
 
-
-def solve(temp, s):
-    low = high = 0
-    curs = 0
-    while low<=high and high<n:
-        a,b,c = temp[low]
-        x,y,z = temp[high]
-        if (b-a+1)*c + (y-x+1)*z 
-
-
-
+inf = 10**10
 t = I()
-
 for _ in range(t):
-    n, q = T()
-    lis = L()
+    n,m = T()
+    line = [0]*(n+2)
+    left = list(range(n+1))
 
-    temp = []
+    for i in range(m): 
+        a,b = T()
+        line[a] += 1
+        line[b+1] -= 1
+        left[b] = min(left[b], a)
 
-    cur = 0
-    while cur<n:
-        last = cur
-        while cur+1<n and lis[cur+1] == lis[cur]: cur+=1
-        temp.append([last, cur, lis[cur]])
-        cur+=1
+    for i in range(1, n+1):
+        line[i] += line[i-1]
 
-    for _ in range(q):
-        p = L()
-        if p[0] == 1:
-            s = p[1]
-            
+    for i in range(n-1,-1,-1):
+        left[i] = min(left[i], left[i+1])
 
+    dp = [0]*(n+1)
+    for i in range(1, n+1):
+        dp[i] = max(dp[left[i]-1] + line[i], dp[i-1])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(dp[-1])
--- contest/contest03.py
+++ contest/contest03.py
@@ -1,42 +1,91 @@
 from debug import *
 
-I =  lambda :int(input())
-L = lambda :list(map(int, input().split()))
-T = lambda :map(int, input().split())
-mod = int(1e9) + 7
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**7)
 
-import random
 
-# n, h, a = I(), L(), L()
-n = 10**5
-h = random.sample(list(range(1, n+1)), n)
-a = random.choices(list(range(10**6)), k=n)
 
-bit = [0]*(n + 10)
-N = n+1
 
-def update(x, d):
-    x+=1
-    while x<N:
-        bit[x] = max(bit[x], d)
-        x += (-x&x)
+# Point class for 
+# 2-D points
 
-def premax(x):
-    x+=1
-    ans = 0
-    while x>0:
-        ans = max(ans, bit[x])
-        x -= (-x&x)
+class point:
+    def __init__(self, x, y) :
+
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+
+def dis(a,b):
+    print(a,b)
+    return (a.x-b.x)**2 + (a.y-b.y)**2
+
+def bruteForce(lis):
+    ans = 10**13
+    for i in range(len(lis)):
+        for j in range(i):
+            ans = min(ans, dis(lis[i], lis[j]))
     return ans
 
-seq = sorted([(h[i], i) for i in range(n)])
-dp = [0]*(n+10)
-ans = 0 
 
-for i in range(n):
-    index = seq[i][1]
-    dp[index] = premax(index-1) + a[index]
-    update(index, dp[index])
-    ans = max(ans, dp[index])
+def closestPair(cordinates, n) :
+    xcords = sorted(cordinates, key=lambda p:p.x)
+    ycords = sorted(cordinates, key=lambda p:p.y)
 
-print(ans)
+    def getStrip(lis, mid, d):
+        store = []
+        for p in lis:
+            if dis(p, point(mid, p.y)) <= d:
+                store.append(p)
+        return store
+
+    def rec(xlis, ylis):
+        if len(xlis) <= 5: return bruteForce(xlis)
+        # split
+        xleft, xright = xlis[:n//2], xlis[n//2:]
+        yleft, yright = [], []
+        mid = xleft[-1]
+        for p in ylis:
+            if p.x <= mid.x: yleft.append(p)
+            else: yright.append(p)
+        
+        d1 = rec(xleft, yleft)
+        d2 = rec(xright, yright)
+        d = min(d1, d2)
+        store = getStrip(ylis, mid, d)
+        final = 10**13
+        for i in range(len(store)):
+            for j in range(1, 16):
+                if i+j<len(store):
+                    final = min(final, dis(store[i], store[i+j]))
+        
+        return min(final, d)
+            
+    # Your code here
+    # return square of minimum distance between n points
+    return rec(xcords, ycords)
+
+
+#taking inpit using fast I/O
+def takeInput() :
+    
+    n = int(input().strip())
+
+    if n == 0 :
+        return list(), n
+
+    cordinates = [point(0,0) for i in range(n)]
+
+    for i in range(n) :
+        arr = list(map(int,stdin.readline().strip().split(" ")))
+        cordinates[i] = point(arr[0], arr[1])
+
+    return cordinates, n
+
+
+#main
+cordinates, n = takeInput()
+print(closestPair(cordinates, n))
